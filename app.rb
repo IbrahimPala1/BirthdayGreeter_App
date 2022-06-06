279b9f88e7b1dd14134e7b930bc2d44140e7b54d
ibrahim pala <ibrahimpala2019@gmail.com>
2022-06-06 11:57:28 +0100

    tests

--- app.rb
+++ app.rb
@@ -1,14 +1,10 @@
 require 'sinatra/base'
-require 'sinatra/reloader'
 
 class BirthdayGreeter < Sinatra::Base
-  configure :development do
-    register Sinatra::Reloader
-  end
 
   get '/' do
-    'Please enter your name'
-  end  
+    erb :index
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
