45f43925e5111335b2d2fec7a50e77686ee4450b
ibrahim pala <ibrahimpala2019@gmail.com>
2022-06-06 22:04:01 +0100

    Created test and implemented infastructure for main page

---
+++ spec/features/index_spec.rb
@@ -0,0 +1,19 @@
+feature "building initial infastrcuture" do 
+
+    scenario "Welcome message" do 
+        visit ('/')
+        expect(page).to have_content 'Welcome to Birthday Greeter'
+    end 
+
+    scenario "submitting names" do 
+        visit('/')
+        expect(page).to have_content 'Enter your name:'
+    end 
+
+    scenario "entering date of birth" do 
+        visit ('/')
+        expect(page).to have_content 'Enter your date of birth:'
+    end 
+end 
+     
+        
